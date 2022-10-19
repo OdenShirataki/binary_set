2cd23aeda0c2b3f04ba7f5836d1cc005f9a39eca
OdenShirataki <git config user.email>
2022-10-19 21:05:57 +0900

    change fn slice to bytes

--- src/lib.rs
+++ src/lib.rs
@@ -7,31 +7,32 @@ use various_data_file::{
 
 pub struct IdxBinary{
     index: IdxSized<DataAddress>
-    ,strings:VariousDataFile
+    ,data:VariousDataFile
 }
 impl IdxBinary{
     pub fn new(path_prefix:&str) -> Result<IdxBinary,std::io::Error>{
         let index=IdxSized::new(&(path_prefix.to_string()+".i"))?;
-        let strings=VariousDataFile::new(&(path_prefix.to_string()+".d"))?;
+        let data=VariousDataFile::new(&(path_prefix.to_string()+".d"))?;
         Ok(IdxBinary{
             index
-            ,strings
+            ,data
         })
     }
-    pub fn str(&self,row:u32)->&str{
+    pub fn bytes(&self,row:u32)->&[u8]{
         match self.index.triee().value(row){
-            Some(word)=>{
-                std::str::from_utf8(self.strings.slice(word)).unwrap()
-            }
-            ,None=>""
+            Some(word)=>self.data.slice(word)
+            ,None=>b""
         }
+    }
+    pub fn str(&self,row:u32)->&str{
+        std::str::from_utf8(self.bytes(row)).unwrap()
     }
     pub fn string(&self,row:u32)->String{
         self.str(row).to_string()
     }
     fn search(&self,target: &[u8])->(Ordering,u32){
         self.index.triee().search_cb(|s|->Ordering{
-            target.cmp(self.strings.slice(s))
+            target.cmp(self.data.slice(s))
         })
     }
     pub fn row(&self,target: &[u8]) -> Option<u32>{
@@ -47,10 +48,11 @@ impl IdxBinary{
         if ord==Ordering::Equal && found_row!=0{
             Some(found_row)
         }else{
-            if let Some(ystr)=self.strings.insert(target){
-                self.index.insert_unique(ystr.address(),found_row,ord,0)
-            }else{
-                None
+            match self.data.insert(target){
+                Some(data)=>self.index.insert_unique(
+                    data.address(),found_row,ord,0
+                )
+                ,None=>None
             }
         }
     }
