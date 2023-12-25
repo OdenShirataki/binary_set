06c21d4fed0a8e17361dd37b7488fb318fe0a649
OdenShirataki <oden.srtk@gmail.com>
2023-12-25 22:14:53 +0900

    refactor!: change returns Option fn bytes.

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,11 @@ pub struct BinarySet {
     data_file: VariousDataFile,
 }
 impl BinarySet {
+    /// Opens the file and creates the BinarySet.
+    /// /// # Arguments
+    /// * `path` - Path of file to save data
+    /// * `allocation_lot` - Extends the specified size when the file size becomes insufficient due to data addition.
+    /// If you expect to add a lot of data, specifying a larger size will improve performance.
     pub fn new<P: AsRef<Path>>(path: P, allocation_lot: u32) -> Self {
         let path = path.as_ref();
         let file_name = path.file_name().map_or("".into(), |f| f.to_string_lossy());
@@ -28,17 +33,17 @@ impl BinarySet {
         }
     }
 
-    pub unsafe fn bytes(&self, row: NonZeroU32) -> &'static [u8] {
-        self.index
-            .value(row)
-            .map_or(b"", |v| self.data_file.bytes(v))
+    /// Returns the value of the specified row. Returns None if the row does not exist.
+    pub fn bytes(&self, row: NonZeroU32) -> Option<&'static [u8]> {
+        self.index.value(row).map(|v| self.data_file.bytes(v))
     }
 
     fn search_end(&self, target: &[u8]) -> Found {
         self.index
-            .search_end(|v| unsafe { self.data_file.bytes(v) }.cmp(target))
+            .search_end(|v| self.data_file.bytes(v).cmp(target))
     }
 
+    /// Search for a sequence of bytes.
     pub fn row(&self, target: &[u8]) -> Option<NonZeroU32> {
         let found = self.search_end(target);
         let found_row = found.row();
@@ -46,6 +51,7 @@ impl BinarySet {
             .then_some(unsafe { NonZeroU32::new_unchecked(found_row) })
     }
 
+    /// Finds a sequence of bytes, inserts it if it doesn't exist, and returns a row.
     pub fn row_or_insert(&mut self, content: &[u8]) -> NonZeroU32 {
         let found = self.search_end(content);
         let found_row = found.row();
